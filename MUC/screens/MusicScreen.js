25f3c6b2c2ac185b46667163dab88f255354f588
Meha Agrawal <agra.meha@hotmail.com>
2019-10-22 00:53:33 -0400

    music screen implemented

--- MUC/screens/MusicScreen.js
+++ MUC/screens/MusicScreen.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Keyboard
+} from "react-native";
 import { WebView } from "react-native-webview";
 
 export default MusicScreen = props => {
+  const [music, onChange] = React.useState("");
+  const [showWeb, onGoPress] = React.useState(false);
+  const [uriString, onChangeUriString] = React.useState(
+    "https://www.youtube.com/results?search_query="
+  );
+  const [musicArr, onChangeMusicArr] = React.useState(["tests"]);
   return (
     <>
       <View>
@@ -10,23 +23,101 @@ export default MusicScreen = props => {
           <Text style={styles.backButton}>{"<"}Back</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.container}>
-        <View style={{ height: 600, width: 300, overflow: "hidden" }}>
-          <WebView source={{ uri: "https://youtube.com" }} />
+      <View style={styles.fullContainer}>
+        <View style={styles.container}>
+          <TextInput
+            style={{
+              height: 40,
+              width: 200,
+              borderColor: "gray",
+              borderWidth: 1,
+              borderRadius: 20,
+              paddingLeft: 20,
+              marginRight: 10
+            }}
+            onChangeText={text => onChange(text)}
+            placeholder="what song?"
+            value={music}
+          />
+          <TouchableOpacity
+            onPress={() =>
+              onPressGo(
+                onGoPress,
+                onChangeMusicArr,
+                music,
+                musicArr,
+                uriString,
+                onChangeUriString
+              )
+            }
+          >
+            <Text style={styles.goButton}>Go</Text>
+          </TouchableOpacity>
         </View>
+        {showWeb && (
+          <View
+            style={{
+              marginTop: 10,
+              height: 510,
+              width: 300,
+              overflow: "hidden"
+            }}
+          >
+            <WebView
+              source={{
+                uri: uriString
+              }}
+            />
+          </View>
+        )}
       </View>
     </>
   );
 };
 
+const onPressGo = (
+  onGoPress,
+  onChangeMusicArr,
+  music,
+  musicArr,
+  uriString,
+  onChangeUriString
+) => {
+  onGoPress(true);
+  Keyboard.dismiss();
+  const words = music.split(" ");
+  onChangeMusicArr(words);
+  changeURI(words, uriString, onChangeUriString);
+};
+
+const changeURI = (musicArr, uriString, onChangeUriString) => {
+  let newUri = uriString;
+  for (let i = 0; i < musicArr.length - 1; i++) {
+    newUri = newUri + "+" + musicArr[i];
+  }
+  newUri = newUri + "+" + musicArr[musicArr.length - 1];
+  onChangeUriString(newUri);
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  fullContainer: {
     justifyContent: "center",
     alignItems: "center"
+  },
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50
   },
   backButton: {
     marginTop: 40,
     marginLeft: 20
+  },
+  goButton: {
+    backgroundColor: "#E5E5E5",
+    padding: 10,
+    borderRadius: 20,
+    marginLeft: 10
   }
 });
